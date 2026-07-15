aeb3ba884bc2bc61885fb3294bba1895930c81eb
Aldi (mbp) <you@example.com>
2026-07-15 19:05:39 +0200

    added multi camera + camera parameters

--- lss/dataset/nuscenes_dataset.py
+++ lss/dataset/nuscenes_dataset.py
@@ -3,7 +3,17 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 from nuscenes.nuscenes import NuScenes
+from pyquaternion import Quaternion
+import numpy as np
 
+CAMERAS = [
+    "CAM_FRONT",
+    "CAM_FRONT_LEFT",
+    "CAM_FRONT_RIGHT",
+    "CAM_BACK",
+    "CAM_BACK_LEFT",
+    "CAM_BACK_RIGHT",
+]
 
 class NuScenesDataset(Dataset):
     def __init__(self, cfg, mode="train", logger=None, version="v1.0-mini"):
@@ -35,26 +45,52 @@ class NuScenesDataset(Dataset):
         self.samples = sorted(
             self.samples, key=lambda x: (x["scene_token"], x["timestamp"])
         )
-
         if logger is not None:
             logger.info(f"{mode}: {len(self.samples)} samples")
 
     def __len__(self):
         return len(self.samples)
 
+
+    def get_camera_data(self, sample):
+        images = {}
+        intrinsics = {}
+        extrinsics = {}
+
+        for cam in CAMERAS:
+            sample_data = self.nusc.get("sample_data", sample["data"][cam])
+            img_path = os.path.join(self.root_dir, sample_data["filename"])
+            images[cam] = read_image(img_path).float() / 255.0
+
+            calib = self.nusc.get(
+                "calibrated_sensor",
+                sample_data["calibrated_sensor_token"]
+            )
+
+            # Intrinsic matrix (3x3)
+            K = torch.tensor(
+                calib["camera_intrinsic"],
+                dtype=torch.float32
+            )
+
+            # Extrinsic matrix Camera -> Ego (4x4)
+            R = Quaternion(calib["rotation"]).rotation_matrix
+            t = np.array(calib["translation"])
+
+            T = np.eye(4)
+            T[:3, :3] = R
+            T[:3, 3] = t
+            intrinsics[cam] = K
+            extrinsics[cam] = torch.tensor(T, dtype=torch.float32)
+        return images, intrinsics, extrinsics
+
     def __getitem__(self, idx):
         sample = self.samples[idx]
 
-        # Front camera
-        cam_token = sample["data"]["CAM_FRONT"]
-        cam_data = self.nusc.get("sample_data", cam_token)
-
-        img_path = os.path.join(self.root_dir, cam_data["filename"])
-
-        image = read_image(img_path).float() / 255.0
+        images, intrinsics, extrinsics = self.get_camera_data(sample)
 
         return {
-            "image": image,
-            "sample_token": sample["token"],
-            "timestamp": sample["timestamp"],
+            "images": images,
+            "intrinsics": intrinsics,
+            "extrinsics": extrinsics,
         }
