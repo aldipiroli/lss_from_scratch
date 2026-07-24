59efdf4a6861af9f79b409d3f3187d1247f02995
Aldi Piroli <aldipiroli@gmail.com>
2026-07-24 21:55:29 +0200

    added scatter features to bev and tests

---
+++ lss/test/test_bev_utils.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import torch
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+from lss.utils.misc import load_config
+from lss.test.test_camera_utils import load_data, CAMERAS
+from lss.utils.bev_utils import project_pcl_to_pillar, scatter_feautres_to_bev
+from lss.utils.camera_utils import (
+    pixel_to_camera_rays,
+    add_depth_along_ray,
+    camera_to_ego,
+)
+
+
+def test_project_pcl_to_pillar():
+    config = load_config("lss/config/nuscenes_mini_config.yaml")
+    B, N, C = 1, 1024, 3
+    pcl = torch.rand(B, N, C)
+    bev_cfg = config["LSS"]["bev_config"]
+    pcl_quant, valid = project_pcl_to_pillar(pcl, bev_cfg)
+    assert pcl_quant.shape == pcl.shape
+
+
+def test_scatter_feautres():
+    config = load_config("lss/config/nuscenes_mini_config.yaml")
+    N, C, C_feat = 1024, 3, 128
+    pcl = torch.rand(N, C)
+    feats = torch.rand(N, C_feat)
+    bev_cfg = config["LSS"]["bev_config"]
+    cell_size = bev_cfg["cell_size"]
+    W = int((bev_cfg["x_max"] - bev_cfg["x_min"]) / cell_size)
+    H = int((bev_cfg["y_max"] - bev_cfg["y_min"]) / cell_size)
+    pcl_quant, valid = project_pcl_to_pillar(pcl, bev_cfg)
+    bev = scatter_feautres_to_bev(feats, pcl_quant, bev_cfg)
+    assert bev.shape == (H, W, C_feat)
+
+
+def test_example_project_pcl_to_pillar():
+    config = load_config("lss/config/nuscenes_mini_config.yaml")
+    images, intrinsics, extrinsics = load_data(npz_path="lss/test/data/sample1.npz")
+    all_r = []
+    all_feats = []
+    for i, cam in enumerate(CAMERAS[:1]):
+        img = images[cam]
+        K = intrinsics[cam]
+        extrinsic = extrinsics[cam]
+        B = img.shape[0]
+
+        r, feats = pixel_to_camera_rays(img, K)
+        r, depths = add_depth_along_ray(r, config["LSS"]["depth_config"])
+        feats = feats[:, :, None, :].repeat(1, 1, len(depths), 1)
+        # flatten depths
+        r = r.reshape(B, -1, r.shape[-1])
+        feats = feats.reshape(B, -1, feats.shape[-1])
+
+        r = camera_to_ego(r, extrinsic)
+        all_r.append(r)
+        all_feats.append(feats)
+
+    all_r = torch.stack(all_r, 0).transpose(1, 0)
+    all_feats = torch.stack(all_feats, 0).transpose(1, 0)
+
+    bev_cfg = config["LSS"]["bev_config"]
+    all_r_quant, valid = project_pcl_to_pillar(all_r, bev_cfg)
+
+    B, n_cam, n_points, n_feats = all_feats.shape
+
+    all_feats = all_feats.reshape(B, n_cam * n_points, n_feats)
+    all_r_quant = all_r_quant.reshape(B, n_cam * n_points, n_feats)
+    valid = valid.reshape(B, n_cam * n_points)
+    all_bevs = []
+    for batch_idx in range(B):
+        curr_feats = all_feats[batch_idx]
+        curr_bev_idx = all_r_quant[batch_idx]
+        curr_valid = valid[batch_idx]
+        bev = scatter_feautres_to_bev(
+            curr_feats[curr_valid], curr_bev_idx[curr_valid], bev_cfg
+        )
+        all_bevs.append(bev)
+    all_bevs = torch.stack(all_bevs, 0)
+
+    cell_size = bev_cfg["cell_size"]
+    W = int((bev_cfg["x_max"] - bev_cfg["x_min"]) / cell_size)
+    H = int((bev_cfg["y_max"] - bev_cfg["y_min"]) / cell_size)
+
+    assert all_bevs.shape == (B, H, W, all_feats.shape[-1])
---
+++ lss/utils/bev_utils.py
@@ -0,0 +1,49 @@
+import torch
+
+
+def project_pcl_to_pillar(pcl, bev_cfg):
+    x_min = bev_cfg["x_min"]
+    x_max = bev_cfg["x_max"]
+    y_min = bev_cfg["y_min"]
+    y_max = bev_cfg["y_max"]
+    cell_size = bev_cfg["cell_size"]
+
+    W = int((x_max - x_min) / cell_size)
+    H = int((y_max - y_min) / cell_size)
+
+    x_idx = torch.floor((pcl[..., 0] - x_min) / cell_size).long()
+    y_idx = torch.floor((pcl[..., 1] - y_min) / cell_size).long()
+
+    valid = (x_idx >= 0) & (x_idx < W) & (y_idx >= 0) & (y_idx < H)
+    pcl_quant = torch.stack((x_idx, y_idx, pcl[..., 2]), dim=-1)
+    return pcl_quant, valid
+
+
+def scatter_feautres_to_bev(feats, bev_idx, bev_cfg):
+    cell_size = bev_cfg["cell_size"]
+    W = int((bev_cfg["x_max"] - bev_cfg["x_min"]) / cell_size)
+    H = int((bev_cfg["y_max"] - bev_cfg["y_min"]) / cell_size)
+    C = feats.shape[-1]
+
+    bev_idx = bev_idx.long()
+    x = bev_idx[..., 0]
+    y = bev_idx[..., 1]
+
+    linear_idx = y * W + x  # (N)
+    linear_idx = linear_idx.unsqueeze(-1).expand(-1, C)  # (N,C)
+
+    bev = torch.zeros(
+        H * W,
+        C,
+        device=feats.device,
+        dtype=feats.dtype,
+    )
+    bev.scatter_reduce_(
+        dim=0,
+        index=linear_idx,
+        src=feats,
+        reduce="sum",
+        include_self=False,
+    )
+    bev = bev.view(H, W, C)
+    return bev
